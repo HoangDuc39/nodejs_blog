2a20b56ee6f493a6acc5873723915c54d1dab321
HoangDuc39 <nghiaduc3901@gmail.com>
2022-11-15 21:20:37 +0700

    Routes and Controller

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,11 @@
+class NewController{
+    //[Get] /news
+    index(req,res){
+        res.render('news');
+    }
+    show(req,res){
+        res.send("New detail!")
+    }
+
+}
+module.exports = new NewController;
--- src/index.js
+++ src/index.js
@@ -7,7 +7,17 @@ const morgan = require('morgan')
 const app = express()
 const port = 3000
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname,'public')))
+
+route(app);
+
+app.use(express.urlencoded({
+  extended:true
+}))
+app.use(express.json())
+
 
 app.use(morgan('combined'))
 app.engine('hbs', engine({
@@ -16,12 +26,7 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources\\views'))
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
